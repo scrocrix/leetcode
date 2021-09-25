dd52bb324a100a47e6dedb0104725a6db916aed0
João Paulo Vieira da Silva <scrocrix@gmail.com>
2021-09-24 22:42:44 -0300

    Improve running median performance

--- problems/medium/find_running_median.go
+++ problems/medium/find_running_median.go
@@ -1,34 +1,79 @@
 package medium
 
 import (
-	"math/big"
-	"sort"
-	"strconv"
+	"container/heap"
+	"fmt"
 )
 
-// O(nˆ2)
+// IntHeap
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// MaxHeap
+type MaxHeap struct {
+	IntHeap
+}
+
+func (h MaxHeap) Less(i, j int) bool {
+	return h.IntHeap[i] > h.IntHeap[j]
+}
+
 func RunningMedian(a []int32) []float64 {
-	var items []int32
+	min := &IntHeap{}
+	heap.Init(min)
+
+	max := &MaxHeap{}
+	heap.Init(max)
+
 	var medians []float64
 
-	for _, num := range a {
-		items = append(items, num)
-
-		sort.Slice(items, func(i, j int) bool {
-			return items[i] < items[j]
-		})
-
-		if len(items) > 0 && len(items)%2 != 0 {
-			m := items[len(items)/2]
-			n, _ := strconv.ParseFloat(big.NewFloat(float64(m)).Text('f', 1), 1)
-			medians = append(medians, n)
-		} else if len(items) > 0 && len(items)%2 == 0 {
-			m1 := items[(len(items)/2)-1]
-			m2 := items[len(items)/2]
-			m := (float64(m1) + float64(m2)) / float64(2)
-			medians = append(medians, m)
+	for _, number := range a {
+		if max.Len() == 0 {
+			heap.Push(max, int(number))
+			medians = append(medians, float64(number))
+			continue
 		}
+
+		heap.Push(min, int(number))
+
+		if min.Len()-max.Len() >= 2 {
+			heap.Push(max, heap.Pop(min).(int))
+		}
+
+		mn := heap.Pop(min).(int)
+		mmn := heap.Pop(max).(int)
+		heap.Push(min, mn)
+		heap.Push(max, mmn)
+
+		medians = append(medians, float64((mn+mmn)/2))
 	}
+
+	fmt.Println("Min:")
+	fmt.Println(min)
+	fmt.Println("Max:")
+	fmt.Println(max)
 
 	return medians
 }
